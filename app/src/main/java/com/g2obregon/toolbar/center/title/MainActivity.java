6c1b3890c6b2cb64a3b73c39bdf4e9df7589f39f
g2o <g2obregon@gmail.com>
2017-05-19 16:16:08 -0400

    Commit all sample source

---
+++ app/src/main/java/com/g2obregon/toolbar/center/title/MainActivity.java
@@ -0,0 +1,27 @@
+package com.g2obregon.toolbar.center.title;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+
+        setSupportActionBar(toolbar);
+        mTitle.setText(toolbar.getTitle());
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+}
